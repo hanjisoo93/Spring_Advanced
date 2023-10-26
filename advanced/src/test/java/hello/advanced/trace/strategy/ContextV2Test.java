677f83c0ba5085b0b31383f207d7b32a00aeb9fe
JisooHan <hanjisoo93@gmail.com>
2023-10-26 18:45:15 +0900

    섹션 3. 템플릿 메서드 패턴
    [Note]
    1. 클린 코드를 위해 핵심기능과 부가기능으로 나누기 : 템프릿으로 만들어서 관리
    
    ** 익명 내부 클래스
    
    2. OrderServiceV0 : 핵심 기능만 있음
    OrderServiceV3 : 핵심 기능 + 부가 기능
    OrderServiceV4 : 핵심 기능 + 템플릿 호출 기능
    
    3. 전략 패턴 : 스프링의 의존관계 주입에서 사용하는 방식! --> 구현체를 변경하거나 새로 만들어도 Context 코드에는 영향을 주지 않음
    
    **GOF 디자인 패턴
    
    4. 콜백

---
+++ advanced/src/test/java/hello/advanced/trace/strategy/ContextV2Test.java
@@ -0,0 +1,49 @@
+package hello.advanced.trace.strategy;
+
+import hello.advanced.trace.strategy.code.strategy.*;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ContextV2Test {
+    /**
+     * 전략 패턴 적용
+     */
+    @Test
+    void strategyV1() {
+        Strategy strategyLogic1 = new StrategyLogic1();
+        ContextV1 context1 = new ContextV1(strategyLogic1);
+        context1.execute();
+
+        Strategy strategyLogic2 = new StrategyLogic2();
+        ContextV1 context2 = new ContextV1(strategyLogic2);
+        context2.execute();
+    }
+
+    /**
+     * 전략 패턴 익명 내부 클래스
+     */
+    @Test
+    void strategyV2() {
+        ContextV2 context = new ContextV2();
+        context.execute(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        });
+        context.execute(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        });
+    }
+
+    @Test
+    void strategyV3() {
+        ContextV2 context = new ContextV2();
+        context.execute(() -> log.info("비즈니스 로직1 실행"));
+        context.execute(() -> log.info("비즈니스 로직2 실행"));
+    }
+}
